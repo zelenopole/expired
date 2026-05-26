0ed9554591abe0fc61a7695c2657e5cf98fb736b
Viktoriia <158566608+zelenopole@users.noreply.github.com>
2026-05-26 11:11:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const draggable = document.querySelectorAll(".draggable");
+let offsetX = 0;
+let offsetY = 0;
+let activeElement = null;
+
+draggable.forEach((draggable) => {
+  draggable.addEventListener("mousedown", (event) => {
+    event.preventDefault();
+    activeElement = draggable;
+    offsetX = event.clientX - draggable.offsetLeft; 
+    offsetY = event.clientY - draggable.offsetTop;
+  });
+});
+
+document.addEventListener("mousemove", () => {
+  if (activeElement) {
+    activeElement.style.left = `${event.clientX - offsetX}px`;
+    activeElement.style.top = `${event.clientY - offsetY}px`;
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  activeElement = null;
+});
+
+const img = document.getElementsByClassName("background-image")[0];
+const poem = document.getElementsByClassName("poem")[0];
+if (poem && img) {
+  poem.addEventListener("scroll", () => {
+    const scrollTop = poem.scrollTop;
+    const maxScroll = poem.scrollHeight - poem.clientHeight;
+    if (maxScroll > 0) {
+      const opacity = scrollTop / maxScroll;
+      img.style.setProperty("--img-opacity", opacity);
+    }
+  });
+}
+
+
